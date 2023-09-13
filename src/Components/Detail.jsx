5d92ffb91711869c564bedc0673548af83abbeab
Jon <jlara2019@gmail.comgit config --global user.email jlara2019@gmail.compwdgit commit>
2023-09-13 07:42:04 -0500

    can add as well as delete

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -17,13 +17,19 @@ const Detail = () => {
     }
 
     getPlayer();
-  }, []);
+  }, [])
+
+  async function handleDelete(){
+    const result = await axios.delete(`https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players/${id}`)
+    console.log(result)
+  }
 
   return (
     <>
       <h1>{player && player.name}</h1>
       <h2>{player && player.breed}</h2>
       <h2>{player && player.status}</h2>
+      <button onClick={handleDelete}>Delete Player</button>
     </>
   );
 };
